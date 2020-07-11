f19d57dc9cd9388e398444e212df316c653bdd7f
Shrikant Patel <shrikantpatel@Shrikants-MacBook-Air.local>
2020-07-10 20:52:39 -0600

    done

---
+++ dummyData.js
@@ -0,0 +1,196 @@
+export const salesData = [
+  {
+    name: "Chandler",
+    purchaseHistory: [
+      {
+        billingAmount: 200,
+        date: "01 May, 2020",
+      },
+      {
+        billingAmount: 100,
+        date: "15 May, 2020",
+      },
+      {
+        billingAmount: 250,
+        date: "02 June, 2020",
+      },
+      {
+        billingAmount: 40,
+        date: "10 June, 2020",
+      },
+      {
+        billingAmount: 55,
+        date: "26 June, 2020",
+      },
+      {
+        billingAmount: 1000,
+        date: "1 July, 2020",
+      },
+      {
+        billingAmount: 200,
+        date: "8 July, 2020",
+      },
+    ],
+  },
+  {
+    name: "Monica",
+    purchaseHistory: [
+      {
+        billingAmount: 200,
+        date: "1 May, 2020",
+      },
+      {
+        billingAmount: 100,
+        date: "15 May, 2020",
+      },
+      {
+        billingAmount: 210,
+        date: "2 June, 2020",
+      },
+      {
+        billingAmount: 30,
+        date: "10 June, 2020",
+      },
+      {
+        billingAmount: 55,
+        date: "26 June, 2020",
+      },
+      {
+        billingAmount: 1000,
+        date: "1 July, 2020",
+      },
+      {
+        billingAmount: 100,
+        date: "8 July, 2020",
+      },
+    ],
+  },
+  {
+    name: "Rachel",
+    purchaseHistory: [
+      {
+        billingAmount: 200,
+        date: "1 May, 2020",
+      },
+      {
+        billingAmount: 10,
+        date: "15 May, 2020",
+      },
+      {
+        billingAmount: 20,
+        date: "2 June, 2020",
+      },
+      {
+        billingAmount: 40,
+        date: "10 June, 2020",
+      },
+      {
+        billingAmount: 55,
+        date: "26 June, 2020",
+      },
+      {
+        billingAmount: 150,
+        date: "1 July, 2020",
+      },
+      {
+        billingAmount: 200,
+        date: "8 July, 2020",
+      },
+    ],
+  },
+  {
+    name: "Joey",
+    purchaseHistory: [
+      {
+        billingAmount: 200,
+        date: "1 May, 2020",
+      },
+      {
+        billingAmount: 100,
+        date: "15 May, 2020",
+      },
+      {
+        billingAmount: 120,
+        date: "2 June, 2020",
+      },
+      {
+        billingAmount: 140,
+        date: "10 June, 2020",
+      },
+      {
+        billingAmount: 55,
+        date: "26 June, 2020",
+      },
+      {
+        billingAmount: 10,
+        date: "1 July, 2020",
+      },
+      {
+        billingAmount: 200,
+        date: "8 July, 2020",
+      },
+    ],
+  },
+  {
+    name: "Ross",
+    purchaseHistory: [
+      {
+        billingAmount: 200,
+        date: "1 May, 2020",
+      },
+      {
+        billingAmount: 100,
+        date: "15 May, 2020",
+      },
+      {
+        billingAmount: 250,
+        date: "2 June, 2020",
+      },
+      {
+        billingAmount: 400,
+        date: "10 June, 2020",
+      },
+      {
+        billingAmount: 55,
+        date: "26 June, 2020",
+      },
+      {
+        billingAmount: 2000,
+        date: "1 July, 2020",
+      },
+      {
+        billingAmount: 200,
+        date: "8 July, 2020",
+      },
+    ],
+  },
+  {
+    name: "Pheobe",
+    purchaseHistory: [
+      {
+        billingAmount: 200,
+        date: "1 May, 2020",
+      },
+      {
+        billingAmount: 100,
+        date: "15 May, 2020",
+      },
+      {
+        billingAmount: 250,
+        date: "2 June, 2020",
+      },
+      {
+        billingAmount: 40,
+        date: "10 June, 2020",
+      },
+      {
+        billingAmount: 1000,
+        date: "1 July, 2020",
+      },
+      {
+        billingAmount: 200,
+        date: "8 July, 2020",
+      },
+    ],
+  },
+];
---
+++ index.js
@@ -0,0 +1,105 @@
+import { salesData as Data } from "./dummyData.js";
+
+class RewardPoints {
+  constructor(data) {
+    this.saleData = data;
+    this.customerData = "";
+    this.months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+  }
+
+  setCustomer(customerName) {
+    let customerData = this.saleData.find(
+      (data) => data.name.toLowerCase() === customerName.toLowerCase()
+    );
+
+    if (!customerData) {
+      return;
+    } else {
+      this.customerData = customerData.purchaseHistory;
+      return `customer set to ${customerName}`;
+    }
+  }
+
+  checkValidMonth(month) {
+    return this.months.find(
+      (monthName) => monthName.toLowerCase() === month.toLowerCase()
+    );
+  }
+
+  monthsPoints(month) {
+    if (!this.checkValidMonth(month)) {
+      return;
+    }
+    return this.pointCalc(this.getMonthData(month), 0);
+  }
+
+  getMonthData(month) {
+    let customerData = this.customerData;
+    return customerData.filter(
+      (data) =>
+        this.months[new Date(data.date).getMonth()].toLowerCase() ===
+        month.toLowerCase()
+    );
+  }
+
+  pointCalc(purchaseData, points) {
+    purchaseData.map((billData) => {
+      let bill = billData.billingAmount;
+      if (bill > 100) {
+        points += (bill - 100) * 2 + 50;
+      } else if (bill <= 100 && bill > 50) {
+        points += bill - 50;
+      }
+    });
+    return points;
+  }
+
+  selectedMonthsPoints(months) {
+    return this.pointCalc(this.getSelectedMonthsData(months), 0);
+  }
+
+  getSelectedMonthsData(months) {
+    let data = [];
+    for (let month of months) {
+      if (!this.checkValidMonth(month)) {
+        console.log(`${month} is invalid`);
+        return;
+      }
+      data = data.concat(this.getMonthData(month));
+    }
+    return data;
+  }
+
+  totalPoints() {
+    return this.pointCalc(this.customerData, 0);
+  }
+}
+
+//customers = 
+
+let data = new RewardPoints(Data);
+let name = "Chandler";
+let singleMonth = "june";
+let months = ["may", "june", "july"];
+
+
+
+console.log(
+  data.setCustomer(name),
+  data.monthsPoints(singleMonth),
+  data.totalPoints(),
+  data.selectedMonthsPoints(months)
+);
